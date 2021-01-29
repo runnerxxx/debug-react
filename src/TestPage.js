e2711cd7f7bbf9eb509224c13cfc4ff297fb705c
徐丽婧 <xulijing@laiye.com>
2021-01-29 14:51:05 +0800

    fix: react源码

--- src/TestPage.js
+++ src/TestPage.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-01-28 12:06:28
+ * @LastEditTime: 2021-01-29 14:45:38
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /debug-react/src/TestPage.js
+ */
 import {React} from './adaptation';
 import B from './B';
 const {useState, useCallback, useEffect, Component} = React;
@@ -16,6 +24,7 @@ export default function TestPage(props) {
     const unmountB = () => {
         setstate(!state);
     }
+    
     return (
         <div className="App">
             HELLO WORLD
